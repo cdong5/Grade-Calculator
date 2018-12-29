9c24e53d825cd9934483e631d7832afddbb366a6
Calvin Dong <38579516+cdong5@users.noreply.github.com>
2018-12-28 23:22:31 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,39 +4,52 @@
 from tkinter import *
 
 def addBox():
-    frame = Frame(root) # Creates a frame for the Entry boxes
+    # Creates a frame for the Entry boxes
+    frame = Frame(root) 
     frame.pack()
-
-    ent1 = Entry(frame) # Inserts entry boxes side by side (Grade, Weight)
+    
+    # Inserts entry boxes side by side (Grade, Weight)
+    ent1 = Entry(frame) 
     ent1.grid(row=0, column=0)
     ent2 = Entry(frame)
     ent2.grid(row=0, column=1)
-
-    all_entries.append((ent1, ent2))  # Appends a list to access info in showEntries()
+    
+    # Appends a list to access info in showEntries()
+    all_entries.append((ent1, ent2))  
 
 def showEntries():
-    weighted = [] # List for weighted value
-    total_ent2 = [] # List for weight value (second entry)
-    for number, (ent1, ent2) in enumerate(all_entries): # Access all Entry values
+    # List for weighted value and second entry
+    weighted = []  
+    total_ent2 = []
+    
+    # Access all Entry values
+    for number, (ent1, ent2) in enumerate(all_entries): 
         a = int(ent1.get())
         b = int(ent2.get())
-        c = a*b # c = the weighted value
-        weighted.append(int(c)) # Appends weighted value for to be put in the formula
-        total_ent2.append(b) # Apends weight value for the formula
-
+        
+        # c = the weighted value
+        c = a*b 
+        
+        # Appends weighted value and weight value to be put in the formula
+        weighted.append(int(c)) 
+        total_ent2.append(b)
+        
+    # If user enters no grades, else, uses average formula
     if len(weighted) == 0:
-        e = 0 # If user enters no grades
+        e = 0 
     else:
-        e = round(sum(weighted) / sum(total_ent2), 2) # Formula for average
-
-    var.set(e) # Updates the Grade label
+        e = round(sum(weighted) / sum(total_ent2), 2) 
+        
+    # Updates the Grade label
+    var.set(e) 
 
 all_entries = []
 root = Tk()
 root.title('Grade Calculator')
 root.geometry("280x280")
 
-topframe = Frame(root) # Frames to organize
+# Frames to organize
+topframe = Frame(root)
 topframe.pack(side=TOP)
 middleframe=Frame(root)
 middleframe.pack(side=TOP)
@@ -48,16 +61,17 @@ gradeweightframe.pack(side=TOP)
 Class_Label = Label(topframe, text="Class: ", bg='yellow').pack(side=LEFT)
 Class_Name = Entry(topframe).pack(side=LEFT)
 
-var = StringVar() # To update the label constantly
-var.set(0) # Default value
+# To update the label constantly, set default as 0
+var = StringVar() 
+var.set(0)
 
 GPA_label=Label(middleframe, text='Grade:', bg='blue', fg='yellow').pack(side=LEFT)
 GPA = Label(middleframe, textvariable = var).pack(side=LEFT)
 
-showButton = Button(buttonframes, text='Calculate', command=showEntries) # Runs function if pressed
+# Runs function if pressed
+showButton = Button(buttonframes, text='Calculate', command=showEntries) 
 showButton.pack()
-
-addboxButton = Button(buttonframes, text='<Add Grade>', fg="Red", command=addBox) # Runs function if pressed
+addboxButton = Button(buttonframes, text='<Add Grade>', fg="Red", command=addBox) 
 addboxButton.pack()
 
 grade_label = Label(gradeweightframe, text="Grade |").pack(side=LEFT)
